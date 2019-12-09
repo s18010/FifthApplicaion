9b81be434e8a3e9192dabf6984a707e1b7417725
s18010 <s18010@std.it-college.ac.jp>
2019-12-09 17:11:57 +0900

    minor change

--- app/src/main/java/com/example/fifthapplicaion/SubFragment.kt
+++ app/src/main/java/com/example/fifthapplicaion/SubFragment.kt
@@ -1,8 +1,10 @@
 package com.example.fifthapplicaion
 
 
+import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.SoundPool
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -37,7 +39,19 @@ class SubFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        soundPool = SoundPool(2, AudioManager.STREAM_ALARM, 0)
+        soundPool =
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                @Suppress("DEPRECATION")
+                SoundPool(2, AudioManager.STREAM_ALARM, 0)
+            } else {
+                val audioAttributes = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                SoundPool.Builder()
+                    .setMaxStreams(1)
+                    .setAudioAttributes(audioAttributes)
+                    .build()
+            }
         systemSound = soundPool.load(context, R.raw.se_maoudamashii_system49, 1)
         chimeSound = soundPool.load(context, R.raw.se_maoudamashii_chime12, 1)
     }
